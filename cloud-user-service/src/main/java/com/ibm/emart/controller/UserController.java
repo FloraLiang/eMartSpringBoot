3973bd4a2d481e7b4426a08a97fbe88bdc3f2b96
Liang Jing <dlliangj@cn.ibm.com>
2020-06-11 22:57:21 +0800

    backend update

--- cloud-user-service/src/main/java/com/ibm/emart/controller/UserController.java
+++ cloud-user-service/src/main/java/com/ibm/emart/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +35,7 @@ import com.ibm.emart.service.UserService;
  */
 @RestController
 @RequestMapping("/api/user")
-// @CrossOrigin(origins="http://localhost:4200")
+ @CrossOrigin(origins="http://localhost:4200")
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -52,11 +53,7 @@ public class UserController {
 	public List<eMartUserEntity> findAllUsers() {
 		return userService.findAllUsers();
 	}
-
-//	@GetMapping("/company/{id}")
-//	public CompanyEntity findCompany(Long id){
-//		return companyServiceClient.queryCompanyEntityById(id);
-//	}
+	
 	@PostMapping("/eMartUser/save")
 	public eMartUserEntity save(eMartUserEntity user){
 		eMartUserEntity res = userRepository.save(user);
